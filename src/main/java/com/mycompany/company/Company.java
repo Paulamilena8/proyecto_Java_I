4a4a0e351864d01cf7b489096269b05a6780e59b
paula <paula@Solecito>
2025-01-22 08:17:08 +0100

    Probando repositorio en github

---
+++ src/main/java/com/mycompany/company/Company.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Company {
+    
+     public static void main(String[] args) {
+        
+       // LoginFrame login = new LoginFrame();
+         
+         
+        new FormularioActualizarSuministros();
+    }
+  
+}
